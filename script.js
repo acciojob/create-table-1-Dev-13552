1e62d9b46f33cca1263af4804531db9c0c5361e5
Dev-13552 <devrathour110@gmail.com>
2025-09-09 21:00:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function insert_Row() {
     //Write your code here
 	let table = document.querySelector("#sampleTable")
-	let row = document.createElement("tr")
-	row.innerHTML = `<td>New Cell1</td>
-					<td>New Cell2</td>`
+	// let row = document.createElement("tr")
+	// row.innerHTML = `<td>New Cell1</td>
+	// 				<td>New Cell2</td>`
 
-	table.insertAdjacentElement('afterbegin', row); 
+	// table.insertAdjacentElement('afterbegin', row); 
+
+	  let newRow = table.insertRow(0);
+
+  // insert two cells
+  let cell1 = newRow.insertCell(0);
+  let cell2 = newRow.insertCell(1);
+
+  // set values
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
   
 }
